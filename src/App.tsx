d5c123e0a47f9c34e6a97df65602c11d1e5052a2
abdullah <hbkamk@gmail.com>
2020-08-14 17:51:47 +0600

    done nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Inject,
+  Edit,
+  EditSettingsModel,
+  Toolbar,
+  ToolbarItems,
+  IEditCell,
+} from "@syncfusion/ej2-react-grids";
+import { MaskedTextBoxComponent } from "@syncfusion/ej2-react-inputs";
+import { Ajax } from "@syncfusion/ej2-base";
+import "./App.css";
 
 function App() {
+  const editOptions: EditSettingsModel = {
+    allowEditing: true,
+    allowAdding: true,
+    allowDeleting: true,
+    mode: "Batch",
+  };
+  const toolbarOptions: ToolbarItems[] = [
+    "Add",
+    "Edit",
+    "Delete",
+    "Update",
+    "Cancel",
+  ];
+  function editTemplate(args: any) {
+    return (
+      <MaskedTextBoxComponent
+        value={args.PhoneNumber}
+        mask="000-000-0000"
+        id="PhoneNumber"
+      />
+    );
+  }
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const ajax = new Ajax(
+      "https://services.odata.org/V4/Northwind/Northwind.svc/Orders",
+      "GET"
+    );
+    ajax.send();
+    ajax.onSuccess = (data: any) => {
+      setData(JSON.parse(data).value);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "10%", marginTop: "5%" }}>
+      <GridComponent
+        dataSource={data}
+        allowPaging={true}
+        pageSettings={{ pageSize: 6 }}
+        editSettings={editOptions}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          <ColumnDirective
+            field="OrderID"
+            headerText="ID"
+            textAlign="Right"
+            width="100"
+            isPrimaryKey={true}
+          />
+          <ColumnDirective
+            field="CustomerID"
+            headerText="EmployeeNo"
+            width="150"
+          />
+          <ColumnDirective
+            field="ShipCountry"
+            headerText="Company"
+            editType="dropdownedit"
+          />
+          <ColumnDirective field="ShipName" headerText="Department" />
+          <ColumnDirective
+            field="Freight"
+            headerText="Salary"
+            textAlign="Right"
+            format="C2"
+            width="150"
+            editType="numericedit"
+          />
+          <ColumnDirective
+            field="OrderDate"
+            headerText="JoiningDate"
+            type="date"
+            format="yMd"
+            width="150"
+            editTemplate={editTemplate}
+          />
+        </ColumnsDirective>
+        <Inject services={[Page, Edit, Toolbar]} />
+      </GridComponent>
     </div>
   );
 }
