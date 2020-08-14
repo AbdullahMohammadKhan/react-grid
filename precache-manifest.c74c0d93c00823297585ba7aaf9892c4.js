cc0f50ad2f483fea4463c9bdac15b4938eabbced
abdullah <hbkamk@gmail.com>
2020-08-14 17:52:49 +0600

    Updates

---
+++ precache-manifest.c74c0d93c00823297585ba7aaf9892c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a3dc656e0866283d98bfa12548ba51",
+    "url": "/react-grid/index.html"
+  },
+  {
+    "revision": "7056e13686dd7ff5bba9",
+    "url": "/react-grid/static/css/main.13bf501e.chunk.css"
+  },
+  {
+    "revision": "fce11b5c5e0532476dd5",
+    "url": "/react-grid/static/js/2.eb984fc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-grid/static/js/2.eb984fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7056e13686dd7ff5bba9",
+    "url": "/react-grid/static/js/main.ab763e17.chunk.js"
+  },
+  {
+    "revision": "4c2edf440848ae478e95",
+    "url": "/react-grid/static/js/runtime-main.7ee37e0f.js"
+  }
+]);
